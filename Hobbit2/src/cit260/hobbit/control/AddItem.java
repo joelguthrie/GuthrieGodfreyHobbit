61611d6874c8ed928f1da76ce2a39f54706fde51
Bryant <Bryant@bryant_pc>
2015-02-14 12:36:12 -0700

    attempted to add AddItem.java
    Needs to be reviewed.

---
+++ Hobbit2/src/cit260/hobbit/control/AddItem.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cit260.hobbit.control;
+
+public getItems(checkInventory);
+
+/**
+ *
+ * @author Bryant
+ */
+public class AddItem {
+    
+    if (checkInventory< 0 or setItems> 10)Then
+		Return -1;
+	Additem = setItems - checkInventory
+Return setItems;
+End
+
+}
